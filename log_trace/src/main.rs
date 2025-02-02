ba86650c414b6037a2f6df025624186e607710c8
Stephen M. Clark <stephen.m.clark@collins.com>
2025-02-02 14:14:51 -0600

    Logger test.

--- log_trace/src/main.rs
+++ log_trace/src/main.rs
@@ -1,5 +1,6 @@
 use tracing::{info, Level};
 use tracing_subscriber::FmtSubscriber;
+use rand::Rng;
 
 fn main() {
     // a builder for `FmtSubscriber`.
@@ -13,11 +14,11 @@ fn main() {
     tracing::subscriber::set_global_default(subscriber)
         .expect("setting default subscriber failed");
 
-    let number_of_yaks = 3;
+    let number_of_yaks = 13;
     // this creates a new event, outside of any spans.
     info!(number_of_yaks, "preparing to shave yaks");
 
-    let number_shaved = yak_shave::shave_all(number_of_yaks);
+    let number_shaved = rand::thread_rng().gen_range(1..=number_of_yaks);
     info!(
         all_yaks_shaved = number_shaved == number_of_yaks,
         "yak shaving completed."
